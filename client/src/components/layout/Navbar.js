d975e1a86f6a1f91f2f44ce800b5c6ff4626fa7e
Jonelle Carless <jonellecarless@Jonelles-MacBook-Pro.local>
2019-04-02 13:36:21 -0400

    update build route on BE server.js file

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,22 +4,61 @@ import { Link, NavLink } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
+//       <header className="toolbar">
+//     <nav className="toolbar__navigation light-green darken-4">
+//       {/* <div /> */}
+//       {/* <div className="toolbar__logo">
+//         <a href="/">
+//         <i className="material-icons">spa</i>
+//         </a>naturalBeauty
+//       </div> */}
+//       <div className="spacer" />
+//       <div className="toolbar_navigation-items">
+//         <ul>
+          
+//         <li key="B">
+//        <NavLink to="/dashboard">Dashboard</NavLink>
+//        </li>
+//        <li key="C">
+//          <NavLink to="/products">Products</NavLink>
+//        </li>
+//        <li key="D">
+//          <NavLink to="/cart">Cart</NavLink>
+//        </li>
+//        <li key="A">
+//           <NavLink to="/">
+//           <i className="material-icons">spa</i>
+//           </NavLink>
+//           </li>
+//        <li key="E">
+//        <span className="float-right">naturalBeauty</span>
+//        </li>
+//         </ul>
+// </div>
+// </nav>
+// </header>
+
       <div className="navbar-fixed">
         <nav className="z-depth-3">
           <div className="nav-wrapper light-green darken-4">
-            <Link
+          <ul>
+            {/* <li key="A">
+              <NavLink to="/">
+              <span>naturalBeauty <i className="material-icons">spa</i></span>
+              </NavLink>
+            </li> */}
+          
+          
+            
+           <Link
               to="/"
-              // style={{
-              //   fontFamily: "Simplifica",
-              //   fontweight: "bold"
-              // }}
               className="col s5 brand-logo right-align white-text"
             >
               <i className="material-icons">spa</i>
               naturalBeauty
             </Link>
 
-            <ul>
+            
       <li key="B">
       <NavLink to="/dashboard">Dashboard</NavLink>
       </li>
--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.use("/api/users", users);
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  // app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
