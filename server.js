d3b3e099bf625cb714802d4da7eaa64493ee6425
Jonelle Carless <33839220+jonellemcarless@users.noreply.github.com>
2019-04-03 11:03:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,12 +19,14 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Bodyparser middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -44,8 +46,8 @@ app.use("/api/users", users);
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === "production") {
-  app.use('/static', express.static(path.join(__dirname, 'public')));
-  // app.use(express.static("client/build"));
+ // app.use('/static', express.static(path.join(__dirname, 'public')));
+ app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
