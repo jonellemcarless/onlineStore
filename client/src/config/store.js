4209a802c9ae62b9f4a77f8c191a9bf8296480ab
Jonelle Carless <33839220+jonellemcarless@users.noreply.github.com>
2019-04-03 12:12:58 -0400

    revert change

--- client/src/config/store.js
+++ client/src/config/store.js
@@ -1,11 +1,5 @@
-import { createStore, combineReducers, compose } from "redux";
+import { createStore, combineReducers } from "redux";
 import cartReducer from "../components/dashboard/cart/reducer"
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-
-
-const initialState = {};
-const middleware = [thunk];
 
 const rootReducer = combineReducers({
   cart: cartReducer
@@ -13,11 +7,8 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
